93197fd854306e1550480fb70753aa2ac2668aa0
bmaqueracho <bmaqueracho@unjbg.edu.pe>
2024-12-04 15:07:43 -0500

    Updates

---
+++ precache-manifest.ae14c7f783da0b9f6fbced5e8adc6011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fcfe676b16f43cadf73262bd6e1735f",
+    "url": "/proyectoIO/index.html"
+  },
+  {
+    "revision": "72d52ba55f6fc4ec6f47",
+    "url": "/proyectoIO/static/css/main.3fc623ca.chunk.css"
+  },
+  {
+    "revision": "0a81de10324efd7d7d4e",
+    "url": "/proyectoIO/static/js/2.e7637465.chunk.js"
+  },
+  {
+    "revision": "5c09c92d92a95affd87aa5ed98bc1737",
+    "url": "/proyectoIO/static/js/2.e7637465.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e6b773a1de3b28772d",
+    "url": "/proyectoIO/static/js/3.f8282c38.chunk.js"
+  },
+  {
+    "revision": "72d52ba55f6fc4ec6f47",
+    "url": "/proyectoIO/static/js/main.4c4d7e7a.chunk.js"
+  },
+  {
+    "revision": "ee8ddc6f02710dd3f517",
+    "url": "/proyectoIO/static/js/runtime-main.810b66c0.js"
+  }
+]);
